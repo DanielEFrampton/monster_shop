f577d61032d7887901a1b510d013f56137525ff0
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-16 21:20:29 -0700

    Add namespaced routes to dashboard index for Admin and Merchant

--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,12 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+
+  namespace :merchant do
+    get '/', to: 'dashboard#index'
+  end
+
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+  end
 end
