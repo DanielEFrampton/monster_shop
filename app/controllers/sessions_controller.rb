55bf4a327b8b51efaabbe5862eb817727d0ed318
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-16 21:59:20 -0700

    Add sad path route for incorrect login info and refactor for length

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    unless login_attempt(params)
+      flash[:bad_credentials] = 'Avast! Thar be problems with the credentials ye input!'
+      redirect_to '/login'
+    end
+  end
+
+private
+
+  def login_attempt(params)
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if !user.nil? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      if user.default?
-        redirect_to '/profile'
-      elsif user.merchant?
-        redirect_to '/merchant'
-      elsif user.admin?
-        redirect_to '/admin'
-      end
+      redirect(user)
+    end
+  end
+
+  def redirect(user)
+    if user.default?
+      redirect_to '/profile'
+    else
+      redirect_to "/#{user.role}"
     end
   end
 end
