efe9b56e2c808d33c065b11516b04deba9e50e6b
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-16 21:21:38 -0700

    Add conditional redirects after login for the three user types

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,13 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/profile'
+      if user.default?
+        redirect_to '/profile'
+      elsif user.merchant?
+        redirect_to '/merchant'
+      elsif user.admin?
+        redirect_to '/admin'
+      end
     end
   end
 end
