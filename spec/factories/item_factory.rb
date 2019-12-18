3efdb392eca85edf992a0c0506be0a599f7dd82b
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-17 17:26:43 -0700

    Extract order factory and item_order factory from item_factory file
    Co-Authored-By: DeMarcus Kirby <KirbyDD@users.noreply.github.com>

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -1,12 +1,4 @@
 FactoryBot.define do
-  factory :order do
-    name { "Pirate Jack" }
-    address { "123 Ocean Breeze" }
-    city { "Bootytown" }
-    state { "Turks & Caicos" }
-    zip { "13375" }
-  end
-
   factory :item do
     name { "Used Pirate Hook" }
     description { "Light wear and tear. Found in the belly of a crocodile." }
@@ -14,12 +6,5 @@ FactoryBot.define do
     image { 'https://smhttp-ssl-42830.nexcesscdn.net/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/f/w/fw9160.jpg' }
     inventory { '5000' }
     merchant
-  end
-
-  factory :item_order do
-    price { 1 }
-    quantity { 1 } 
-    item
-    order
   end
 end
