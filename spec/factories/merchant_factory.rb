82a950ca6911392047889a455a6840dca6ae64b5
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-17 17:03:57 -0700

    Add factory for merchant data
    Co-Authored-By: DeMarcus Kirby <KirbyDD@users.noreply.github.com>

---
+++ spec/factories/merchant_factory.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :merchant do
+    name { "Pirate Jack's Bizarro Emporium" }
+    address { "123 Ocean Breeze" }
+    city { "Bootytown" }
+    state { "Turks & Caicos" }
+    zip { "13375" }
+  end
+end
