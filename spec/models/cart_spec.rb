e96c35e9b002b1cb53aeff793916a9e07348d135
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-18 12:27:35 -0700

    Add unit tests for all pre-existing methods on Cart PORO

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -12,5 +12,46 @@ RSpec.describe Cart, type: :model do
         expect(cart.contents[item.id]).to eq(2)
       end
     end
+
+    describe 'subtract_quantity' do
+      it 'decrements quantity of item in cart by one' do
+        item = create(:item)
+        cart = Cart.new({item.id => 5})
+
+        cart.subtract_quantity(item.id)
+
+        expect(cart.contents[item.id]).to eq(4)
+      end
+    end
+
+    describe 'limit_reached?' do
+      it 'checks if item quantity in cart is equal to item inventory' do
+        item = create(:item, inventory: 5)
+        cart = Cart.new({item.id => 4})
+
+        expect(cart.contents[item.id]).to eq(4)
+        expect(cart.limit_reached?(item.id)).to eq(false)
+
+        cart.add_quantity(item.id)
+
+        expect(cart.contents[item.id]).to eq(5)
+        expect(cart.limit_reached?(item.id)).to eq(true)
+      end
+    end
+
+    describe 'quantity_zero?' do
+      it 'checks if quantity of item in cart is zero' do
+        item = create(:item, inventory: 5)
+        cart = Cart.new({item.id => 1})
+
+        expect(cart.contents[item.id]).to eq(1)
+        expect(cart.quantity_zero?(item.id)).to eq(false)
+
+        cart.subtract_quantity(item.id)
+
+        expect(cart.contents[item.id]).to eq(0)
+        expect(cart.quantity_zero?(item.id)).to eq(true)
+      end
+    end
   end
 end
