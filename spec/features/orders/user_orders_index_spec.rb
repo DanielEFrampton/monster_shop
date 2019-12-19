83014450b2db79f2eccd9797dd96b7a192c48522
Phil DeLong <philjdelong@gmail.com>
2019-12-19 13:45:41 -0700

    Update test for story 28

--- spec/features/orders/user_orders_index_spec.rb
+++ spec/features/orders/user_orders_index_spec.rb
@@ -3,23 +3,36 @@ require 'rails_helper'
 RSpec.describe "As a User" do
   describe "when i visit the profile page", type: :feature do
     before :each do
-      @default_user = User.create(  name: "Pirate Jack",
-                                    address: "123 Ocean Breeze",
-                                    city: "Bootytown",
-                                    state: "Turks & Caicos",
-                                    zip: "13375",
-                                    email: "pirate@thecarribean.com",
-                                    password: "landlubberssuck",
-                                    role: 0)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@default_user)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
+      @user = create(:user)
+
+      @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      @order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
       visit '/profile'
+      click_link 'Orders'
     end
 
     it "i can see a link to the orders index page" do
-      click_link 'Orders'
       expect(current_path).to eq("/profile/orders")
+    end
+
+    it "i can see all orders and information" do
+      expect(page).to have_link("#{@order_1.id}")
+      expect(page).to have_content(@order_1.created_at)
+      expect(page).to have_content(@order_1.updated_at)
+      expect(page).to have_content(@order_1.status)
+      expect(page).to have_content(@order_1.total_quantity)
+      expect(page).to have_content(@order_1.grand_total)
     end
   end
 end
