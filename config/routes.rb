8ed52facd44dc89c8cc978635e402312b5008162
DeMarcus Kirby <tkd024@gmail.com>
2019-12-20 09:38:34 -0700

    Add routes for edit and update of user

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/register", to: "users#create"
   get "/profile", to: "users#show"
+  get "/profile/edit", to: "users#edit"
+  patch "/profile", to: "users#update"
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
