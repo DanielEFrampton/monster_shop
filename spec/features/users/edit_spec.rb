73c9cc327c17ae2c9c4af7952fac5fa711c997f3
DeMarcus Kirby <tkd024@gmail.com>
2019-12-20 09:38:12 -0700

    Add additional steps to test to make sure old ifnormation no longer shows

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe 'As a user editing my profile', type: :feature do
 
   it 'Should update user information' do
-      default_user = User.create(name: "Pirate Jack",
+      default_user = User.create!(name: "Pirate Jack",
                                  address: "123 Ocean Breeze",
                                     city: "Bootytown",
                                     state: "Turks & Caicos",
                                     zip: "13375",
                                     email: "pirate@thecarribean.com",
                                     password: "landlubberssuck",
+                                    password_confirmation: "landlubberssuck",
                                     role: 0)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user)
@@ -37,12 +38,18 @@ RSpec.describe 'As a user editing my profile', type: :feature do
 
       expect(current_path).to eq("/profile")
 
+      expect(page).to have_content("Your information has been updated.")
       expect(page).to have_content(default_user.name)
       expect(page).to have_content("7 Seas Drive")
       expect(page).to have_content("Port Saint Kitts")
       expect(page).to have_content("Arrrkansas")
       expect(page).to have_content("13376")
       expect(page).to have_content(default_user.email)
+
+      expect(page).to_not have_content("123 Ocean Breeze")
+      expect(page).to_not have_content("Bootytown")
+      expect(page).to_not have_content("Turks & Caicos")
+      expect(page).to_not have_content("13375")
     end
 
 end
