53b8d5e341c4ff60de07055ba16d54ac73c2edfe
Phil DeLong <philjdelong@gmail.com>
2020-01-07 11:49:57 -0700

    Write conditional for flash messages

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -7,6 +7,11 @@ class Merchant::ItemsController < Merchant::BaseController
   def update
     item = Item.find(params[:id])
     item.toggle!(:active?)
+    if item.active?
+      flash[:notice] = "Yer item is active! Now go get the booty.."
+    else
+      flash[:notice] = "Yer item has scurvy. Inactive!"
+    end
     redirect_to '/merchant/items'
   end
 end
