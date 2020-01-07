c4ed41624a8d8ccdda42375fdf8c4136da57a14a
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-07 11:28:10 -0700

    Move item_orders controller to merchant namespace

--- app/controllers/merchant/item_orders_controller.rb
+++ app/controllers/merchant/item_orders_controller.rb
@@ -1,15 +1,9 @@
-class ItemOrdersController < ApplicationController
+class Merchant::ItemOrdersController < Merchant::BaseController
   def update
     item_order = ItemOrder.find(params[:id])
     item_order.toggle!(:fulfilled)
-    item_order.item.update!(inventory: new_quantity(item, item_order))
+    item_order.item.update!(inventory: item_order.item.inventory - item_order.quantity)
     flash[:fulfilled] = "Yarrrgh. I guess I'll split me booty wid ye. Yer order be fulfilled."
     redirect_to "/merchant/orders/#{item_order.order.id}"
-  end
-
-  private
-
-  def new_quantity(item, item_order)
-    item.inventory - item_order.quantity
   end
 end
