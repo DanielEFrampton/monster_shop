09e40f8001c081747c4f732f0f6e77b615b5b092
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-07 11:13:21 -0700

    Refactor item_orders update action to remove save steps

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -1,10 +1,8 @@
 class ItemOrdersController < ApplicationController
   def update
     item_order = ItemOrder.find(params[:id])
-    item_order.toggle(:fulfilled).save
-    item = item_order.item
-    item.update(inventory: new_quantity(item, item_order))
-    item.save
+    item_order.toggle!(:fulfilled)
+    item_order.item.update!(inventory: new_quantity(item, item_order))
     flash[:fulfilled] = "Yarrrgh. I guess I'll split me booty wid ye. Yer order be fulfilled."
     redirect_to "/merchant/orders/#{item_order.order.id}"
   end
