4c0a881d6ed7c1db1e5bb23cad3edc0ae0254523
Phil DeLong <philjdelong@gmail.com>
2020-01-07 16:58:11 -0700

    Create edit and update methods for merchant items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,7 +4,23 @@ class Merchant::ItemsController < Merchant::BaseController
     @merchant = Merchant.first
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      flash[:success] = "Yer booty has been corrected!"
+      redirect_to '/merchant/items'
+    else
+      flash[:error] = item.errors.full_messages.to_sentence
+      redirect_to "/merchant/items/#{item.id}/edit"
+    end
+  end
+
+  def activate
     item = Item.find(params[:id])
     item.toggle!(:active?)
     if item.active?
@@ -13,5 +29,11 @@ class Merchant::ItemsController < Merchant::BaseController
       flash[:notice] = "Yer item has scurvy. Inactive!"
     end
     redirect_to '/merchant/items'
+  end
+
+  private
+
+  def item_params
+    params.permit(:name,:description,:price,:inventory,:image)
   end
 end
