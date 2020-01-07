e6c1c8cf2873d1712d51eab3dc8c8d9ca6ea38f8
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-06 20:24:36 -0700

    Add enable and disable methods to admin/merchants_controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,4 +4,19 @@ class Admin::MerchantsController < Admin::BaseController
     @merchant = Merchant.find(params[:id])
   end
 
+  def enable
+    merchant = Merchant.find(params[:id])
+    merchant.update(disabled: false)
+    merchant.items.update_all(disabled: false)
+    flash[:notice] = 'Ahoy! This merchant has re-joined the ranks.'
+    redirect_to "/merchants"
+  end
+
+  def disable
+    merchant = Merchant.find(params[:id])
+    merchant.update(disabled: true)
+    merchant.items.update_all(disabled: true)
+    flash[:notice] = 'This merchant has walked the plank.'
+    redirect_to "/merchants"
+  end
 end
