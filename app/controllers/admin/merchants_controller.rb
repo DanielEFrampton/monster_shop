a672db91d08deba86e204250a26b931e15671fba
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-07 11:12:50 -0700

    Refactor enable and disable methods into single update method

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,19 +4,16 @@ class Admin::MerchantsController < Admin::BaseController
     @merchant = Merchant.find(params[:id])
   end
 
-  def enable
-    merchant = Merchant.find(params[:id])
-    merchant.update(disabled: false)
-    merchant.items.update_all(disabled: false)
-    flash[:notice] = 'Ahoy! This merchant has re-joined the ranks.'
-    redirect_to "/merchants"
-  end
-
-  def disable
-    merchant = Merchant.find(params[:id])
-    merchant.update(disabled: true)
-    merchant.items.update_all(disabled: true)
-    flash[:notice] = 'This merchant has walked the plank.'
-    redirect_to "/merchants"
+  def update
+    @merchant = Merchant.find(params[:id])
+    if params[:enable_disable] == 'enable'
+      @merchant.enable
+      flash[:notice] = 'Ahoy! This merchant has re-joined the ranks.'
+      redirect_to "/merchants"
+    else
+      @merchant.disable
+      flash[:notice] = 'This merchant has walked the plank.'
+      redirect_to "/merchants"
+    end
   end
 end
