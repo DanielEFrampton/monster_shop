af9b7721f55c34780c655f05ef3e0ab76162846d
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-07 21:26:25 -0700

    Change migration and schema to add default image url for new items

--- db/migrate/20190817190207_create_items.rb
+++ db/migrate/20190817190207_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :description
       t.integer :price
-      t.string :image
+      t.string :image, default: 'https://res.cloudinary.com/teepublic/image/private/s--jAfsTCc0--/t_Preview/b_rgb:42332c,c_limit,f_jpg,h_630,q_90,w_630/v1472219144/production/designs/651797_1.jpg'
       t.boolean :active?, default: true
       t.integer :inventory
       t.references :merchant, foreign_key: true
