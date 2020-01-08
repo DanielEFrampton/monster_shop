e7f3fec509f8333ec6d8d98c10eb6de473e87bfa
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-08 11:59:47 -0700

    Test partial

---
+++ spec/features/admin/users_show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'admin users show page' do
+  before(:each) do
+    @admin_user_1 = create(:user, role: 2)
+    @default_user_1 = create(:user, role: 0)
+    @default_user_2 = create(:user, role: 0)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin_user_1)
+  end
+
+  it 'can see all info a user can see except edit button' do
+    visit "/admin/users/#{@default_user_1.id}"
+
+    expect(page).to have_content(@default_user_1.name)
+    expect(page).to have_content(@default_user_1.address)
+    expect(page).to have_content(@default_user_1.city)
+    expect(page).to have_content(@default_user_1.state)
+    expect(page).to have_content(@default_user_1.zip)
+    expect(page).to have_content(@default_user_1.email)
+
+    expect(page).to_not have_link("Update Profile")
+
+    visit "/admin/users/#{@default_user_2.id}"
+
+    expect(page).to have_content(@default_user_2.name)
+    expect(page).to have_content(@default_user_2.address)
+    expect(page).to have_content(@default_user_2.city)
+    expect(page).to have_content(@default_user_2.state)
+    expect(page).to have_content(@default_user_2.zip)
+    expect(page).to have_content(@default_user_2.email)
+
+    expect(page).to_not have_link("Update Profile")
+  end
+end
