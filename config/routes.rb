9adabb831e0b233b6b31eed1e625b8a02262dcd3
DeMarcus Kirby <tkd024@gmail.com>
2020-01-08 14:34:00 -0700

    Refactor routes to utilize resources and DRY code
    Co-Authored-By: Melissa Robbins <mel-rob@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "home#index"
-
-  # get "/merchants", to: "merchants#index"
-  # get "/merchants/new", to: "merchants#new"
-  # get "/merchants/:id", to: "merchants#show"
-  # post "/merchants", to: "merchants#create"
-  # get "/merchants/:id/edit", to: "merchants#edit"
-  # patch "/merchants/:id", to: "merchants#update"
-  # delete "/merchants/:id", to: "merchants#destroy"
 
   resources :merchants
+  resources :reviews, only: [:edit, :update, :destroy]
+  resources :orders, only: [:new, :update]
+  resources :items do
+    resources :reviews, only: [:new, :create]
+  end
 
-  # get "/items", to: "items#index"
-  # get "/items/:id", to: "items#show"
-  # get "/items/:id/edit", to: "items#edit"
-  # patch "/items/:id", to: "items#update"
-  # get "/merchants/:merchant_id/items", to: "items#index"
-  # get "/merchants/:merchant_id/items/new", to: "items#new"
-  # post "/merchants/:merchant_id/items", to: "items#create"
-  # delete "/items/:id", to: "items#destroy"
-  resources :items
-
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  get "/", to: "home#index"
+  get "/merchants/:merchant_id/items", to: "items#index"
+  get "/merchants/:merchant_id/items/new", to: "items#new"
+  post "/merchants/:merchant_id/items", to: "items#create"
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
@@ -34,10 +18,8 @@ Rails.application.routes.draw do
   delete "/cart", to: "cart#empty"
   delete "/cart/:item_id", to: "cart#remove_item"
 
-  get "/orders/new", to: "orders#new"
   post "/users/:id/orders", to: "orders#create"
   get "/profile/orders/:order_id", to: "orders#show"
-  patch "/orders/:id", to: 'orders#update'
   get '/profile/orders', to: 'orders#index'
 
   get "/register", to: "users#new"
@@ -52,13 +34,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-
   namespace :merchant do
     get '/', to: 'dashboard#index'
-
-    # get '/items/:id', to: 'items#edit'
     patch '/items/:id/activation', to: 'items#activate'
-
     resources :items, only: [:index, :new, :create, :edit, :update]
     resources :item_orders, only: [:update]
     resources :orders, only: [:show]
