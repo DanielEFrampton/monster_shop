66d9ed385fd6a8b9778f1a8df825f497ec5f2419
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-08 15:56:03 -0700

    Remove deprecated edit, update, destroy actions on items and merchants

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,28 +28,6 @@ class ItemsController<ApplicationController
     end
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to "/items/#{@item.id}"
-    else
-      flash[:error] = @item.errors.full_messages.to_sentence
-      render :edit
-    end
-  end
-
-  def destroy
-    item = Item.find(params[:id])
-    Review.where(item_id: item.id).destroy_all
-    item.destroy
-    redirect_to "/items"
-  end
-
   private
 
   def item_params
