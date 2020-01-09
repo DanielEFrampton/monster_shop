1f0d12ec5fbf12b2c24d11d749f68985a5935fef
DeMarcus Kirby <tkd024@gmail.com>
2020-01-09 15:17:28 -0700

    Add config to recompile the audios folder

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module MonsterShopStarter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.precompile << "audios/*"
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
