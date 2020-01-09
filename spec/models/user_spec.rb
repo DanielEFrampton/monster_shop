d73dc32e18378cf4b788eafb19a26768026a54b1
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-09 16:14:22 -0700

    Fix validation for password_digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :zip}
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
-    it {should validate_presence_of :password}
+    it {should validate_presence_of :password_digest}
 
     it {should have_many :orders}
   end
