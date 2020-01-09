733dab052b5e03dd7b96d91840ec8165739cbe42
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-09 15:14:44 -0700

    Add routes for edit, update, edit_password, and update_password

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :items, except: [:new, :create] do
     resources :reviews, only: [:new, :create]
   end
-  resources :merchants 
+  resources :merchants
 
   get "/", to: "home#index"
   get "/merchants/:merchant_id/items", to: "items#index"
@@ -46,6 +46,8 @@ Rails.application.routes.draw do
     resources :merchants, only: [:show]
     get '/', to: 'dashboard#index'
     patch '/merchants/:id/:enable_disable', to: 'merchants#update'
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show, :edit, :update]
+    get '/users/:id/edit_password', to: 'users#edit_password'
+    patch '/users/:id/edit_password', to: 'users#update_password'
   end
 end
