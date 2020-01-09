fc6639250f157ebd1029539f6e9fe3355c4943e0
Melissa Robbins <melissamrobbins@gmail.com>
2020-01-09 15:15:56 -0700

    Remove validation requirement test for password_confirmation in User model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password}
-    it {should validate_presence_of :password_confirmation}
 
     it {should have_many :orders}
   end
