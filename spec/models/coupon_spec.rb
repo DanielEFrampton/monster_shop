c3d3b0e5d2f23c47416e96f078b9876099c75e7f
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-11 14:20:32 -0700

    Modify Coupon model test to expect percent_off to be float

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe Coupon, type: :model do
     it { should validate_presence_of :code }
     it { should validate_uniqueness_of :code }
     it { should validate_presence_of :percent_off }
-    it { should validate_inclusion_of(:percent_off).in_range(0..100) }
+    it { should validate_inclusion_of(:percent_off).in_range(0.0..1.0) }
   end
 
   describe 'initalization' do
     before(:each) do
-      @coupon = Coupon.new(name: "50%-off Coupon", code: "ABC123", percent_off: 50)
+      @coupon = Coupon.new(name: "50%-off Coupon", code: "ABC123", percent_off: 0.5)
     end
 
     it "should initialize with provided name, code, and percent off" do
       expect(@coupon.name).to eq("50%-off Coupon")
       expect(@coupon.code).to eq("ABC123")
-      expect(@coupon.percent_off).to eq(50)
+      expect(@coupon.percent_off).to eq(0.5)
     end
 
     it "should initialize with enabled field with default value of true" do
