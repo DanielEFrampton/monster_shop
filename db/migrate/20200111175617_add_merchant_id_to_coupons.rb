605b77f0c2ff183f92ace2e79dbf30e7d677f00a
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-11 10:57:38 -0700

    Create and run migration to add merchant_id references field to Coupons

---
+++ db/migrate/20200111175617_add_merchant_id_to_coupons.rb
@@ -0,0 +1,5 @@
+class AddMerchantIdToCoupons < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :coupons, :merchant, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200111173232) do
+ActiveRecord::Schema.define(version: 20200111175617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20200111173232) do
     t.boolean "enabled", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
   create_table "item_orders", force: :cascade do |t|
@@ -97,6 +99,7 @@ ActiveRecord::Schema.define(version: 20200111173232) do
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
+  add_foreign_key "coupons", "merchants"
   add_foreign_key "item_orders", "items"
   add_foreign_key "item_orders", "orders"
   add_foreign_key "items", "merchants"
