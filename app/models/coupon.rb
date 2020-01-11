5735d05ad31f232636e4585bb1e5b9ae3c9bb7ab
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-11 10:46:57 -0700

    Add validatations to Coupon model to pass validations model tests

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,5 @@
 class Coupon < ApplicationRecord
-
+  validates_presence_of :name, :code, :percent_off
+  validates_uniqueness_of :name, :code
+  validates_inclusion_of :percent_off, in: (0..100)
 end
