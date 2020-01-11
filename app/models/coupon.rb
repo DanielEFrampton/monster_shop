15768025d99a6ce18ce2c3811083f1553cb519d5
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-11 10:57:18 -0700

    Add belongs_to relationship with merchant to Coupon model

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,4 +2,6 @@ class Coupon < ApplicationRecord
   validates_presence_of :name, :code, :percent_off
   validates_uniqueness_of :name, :code
   validates_inclusion_of :percent_off, in: (0..100)
+
+  belongs_to :merchant
 end
