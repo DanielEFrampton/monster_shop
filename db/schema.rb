6dd29fde084dfeb861bea435ca065c009b7ace2e
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-11 10:37:58 -0700

    Create schema by running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106203600) do
+ActiveRecord::Schema.define(version: 20200111173232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.integer "percent_off"
+    t.boolean "enabled", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "item_orders", force: :cascade do |t|
     t.bigint "order_id"
