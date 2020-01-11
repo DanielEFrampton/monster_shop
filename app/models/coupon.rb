bdf8919a14cb30c9136000750caad5466b705f7b
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-11 14:22:11 -0700

    Modify Coupon validate_inclusion method to look in range from 0.0 to 1.0

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,7 @@
 class Coupon < ApplicationRecord
   validates_presence_of :name, :code, :percent_off
   validates_uniqueness_of :name, :code
-  validates_inclusion_of :percent_off, in: (0..100)
+  validates_inclusion_of :percent_off, in: (0.0..1.0)
 
   belongs_to :merchant
 end
