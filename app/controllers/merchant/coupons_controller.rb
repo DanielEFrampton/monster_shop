64011219e547f768b5388351a11c553f8308c72f
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-13 05:28:19 -0700

    Add new and create actions to Merchant Coupons controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -2,4 +2,10 @@ class Merchant::CouponsController < Merchant::BaseController
   def index
     @coupons = Merchant.find(current_user.merchant.id).coupons
   end
+
+  def new
+  end
+
+  def create
+  end
 end
