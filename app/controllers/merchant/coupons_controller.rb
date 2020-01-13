c8ff5609e1786a025ff2115b9946ec79deec87ab
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-13 06:02:12 -0700

    Add content to merchant coupons new and create actions

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -8,5 +8,17 @@ class Merchant::CouponsController < Merchant::BaseController
   end
 
   def create
+    @coupon = current_user.merchant.coupons.new(coupon_params)
+    if @coupon.save
+      redirect_to '/merchant/coupons'
+    else
+      flash[:error] = @coupon.errors.full_messages.to_sentence
+      render :new
+    end
   end
+
+  private
+    def coupon_params
+      params.require('coupon').permit(:name, :code, :percent_off)
+    end
 end
