b6447066586a9e3d482a8fa3934564da779d1485
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-13 06:54:30 -0700

    Correct class name of migration to change percent_off to integer

--- db/migrate/20200113130612_change_percent_off_to_integer_in_coupons.rb
+++ db/migrate/20200113130612_change_percent_off_to_integer_in_coupons.rb
@@ -1,4 +1,4 @@
-class ChangePercentOffToDecimalInCoupons < ActiveRecord::Migration[5.1]
+class ChangePercentOffToIntegerInCoupons < ActiveRecord::Migration[5.1]
   def change
     change_column :coupons, :percent_off, :integer
   end
