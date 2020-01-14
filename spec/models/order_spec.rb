683c6cea6df4d3a66134f35825cf0b9585c84bc7
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-14 09:20:18 -0700

    Add expectation to order model test of optional belongs_to to coupon

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,9 +10,10 @@ describe Order, type: :model do
   end
 
   describe "relationships" do
-    it {should have_many :item_orders}
-    it {should have_many(:items).through(:item_orders)}
-    it {should belong_to :user}
+    it { should have_many :item_orders }
+    it { should have_many(:items).through(:item_orders) }
+    it { should belong_to :user }
+    it { should belong_to(:coupon).optional }
   end
 
   describe 'statuses' do
