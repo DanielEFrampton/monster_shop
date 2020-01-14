99251410acaafd405e07e277909978af529dbe56
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-14 09:20:33 -0700

    Add optional belongs_to relationship from order to coupon

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,9 @@
 class Order <ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
-
-  belongs_to :user
-
   has_many :item_orders
   has_many :items, through: :item_orders
   belongs_to :user
+  belongs_to :coupon, optional: true
 
   enum status: %w(pending packaged shipped cancelled)
 
